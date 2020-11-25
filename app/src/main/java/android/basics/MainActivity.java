08fe58077f32d70259b86c984ae09a0d555ad3f1
Dominik277 <dominikdoric277@gmail.com>
2020-11-25 10:40:06 +0100

    ActionBar App.Dodana dva gumba u action bar te nam ispisuje Toast poruku ovisno o tome koji gumb smo kliknuli.

--- app/src/main/java/android/basics/MainActivity.java
+++ app/src/main/java/android/basics/MainActivity.java
@@ -1,8 +1,15 @@
 package android.basics;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.toastMenu:
+                Toast.makeText(this,"Kliknuo si lijevi gumb",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.launchMenu:
+                Toast.makeText(this,"Kliknuo si desni gumb",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
