ead2f0628105eacb0ec66dbc28c144401dad353c
Dominik277 <dominikdoric277@gmail.com>
2020-11-26 21:59:50 +0100

    Async Task Perform.

---
+++ app/src/main/java/android/basics/AsyncTaskPerformActivity.java
@@ -0,0 +1,44 @@
+package android.basics;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AsyncTaskPerformActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_task_perform);
+        new MyAsyncTask().execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_async_task_perform,menu);
+        return true;
+    }
+
+    public void doneCounting(){
+        Toast.makeText(this,"Done counting to 100000",Toast.LENGTH_LONG).show();
+    }
+
+    private class MyAsyncTask extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            for (long i=0; i<100000; i++){
+                System.out.println(i);
+            }
+            return null;
+        }
+        protected void onPostExecute(Void result){
+            doneCounting();
+        }
+    }
+}
