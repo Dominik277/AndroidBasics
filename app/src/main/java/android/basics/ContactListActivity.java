d746873ffecda08e0286a56a7b2204545dfe58f2
Dominik277 <dominikdoric277@gmail.com>
2020-11-26 11:33:42 +0100

    Contact List Activity.

--- app/src/main/java/android/basics/ContactListActivity.java
+++ app/src/main/java/android/basics/ContactListActivity.java
@@ -1,18 +1,21 @@
 package android.basics;
 
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
 public class ContactListActivity extends AppCompatActivity {
 
@@ -48,6 +51,35 @@ public class ContactListActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    private void loadContacts(){
+        Uri allContacts = Uri.parse("content://contacts/people");
+        CursorLoader cursorLoader = new CursorLoader(this,allContacts,
+                null,
+                null,
+                null,
+                null
+                );
+
+        Cursor cursor = cursorLoader.loadInBackground();
+        if (cursor.moveToFirst()){
+            do{
+               int idIndex = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+               String contactID = cursor.getString(idIndex);
+
+               int nameIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+               String contactDisplayName = cursor.getString(nameIndex);
+               names.add(contactDisplayName);
+            }while (cursor.moveToNext());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_list, menu);
+        return true;
     }
 
 }
