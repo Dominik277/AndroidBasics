a4dfddbca029d399a212b8442513521e92925fba
Dominik277 <dominikdoric277@gmail.com>
2020-11-27 20:01:00 +0100

    Dodan vizualni dio za ExplicitIntentActivity(zavrsno).

--- app/src/main/java/android/basics/ExplicitIntentActivity.java
+++ app/src/main/java/android/basics/ExplicitIntentActivity.java
@@ -1,7 +1,11 @@
 package android.basics;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +15,21 @@ public class ExplicitIntentActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        Button btnLaunchedSecond = findViewById(R.id.btnLaunchSecond);
+        btnLaunchedSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ExplicitIntentActivity.this,SimpleBundleDemoActivity.class);
+                i.putExtra("Text","Passed String extra");
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_explicit_intent,menu);
+        return true;
     }
 }
