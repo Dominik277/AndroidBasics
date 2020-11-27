8a9bbca32f2cb07710a2c9a42e1d2d73bfb513e0
Dominik277 <dominikdoric277@gmail.com>
2020-11-27 09:03:49 +0100

    Dodana ChaptersListAdapter klasa koja nasljeđuje BaseExpandableListAdapter te implementirane sve metode koje ona ima.

--- app/src/main/java/android/basics/DemoSelectorActivity.java
+++ app/src/main/java/android/basics/DemoSelectorActivity.java
@@ -1,4 +1,88 @@
 package android.basics;
 
-public class DemoSelectorActivity {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DemoSelectorActivity extends AppCompatActivity {
+
+
+
+
+
+
+
+    private class ChaptersListAdapter extends BaseExpandableListAdapter{
+
+    private String[] chapters = getResources().getStringArray(R.array.chapters);
+    private String[][] exercises;
+
+    public ChaptersListAdapter(){
+        super();
+        exercises = new String[chapters.length][];
+        for (int i=0; i<exercises.length; i++){
+            int resId = getResources().getIdentifier
+                    ("chap" + (i+1),"array",getPackageName());
+            exercises[i] = getResources().getStringArray(resId);
+        }
+    }
+
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
 }
+
+}
+
+
+
+
