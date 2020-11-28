4545846dfd1ad47c934fa55c14cfb94e8ead7745
Dominik277 <dominikdoric277@gmail.com>
2020-11-28 22:32:35 +0100

    SimpleReturnResultActivity dodana sendResult metoda.

--- app/src/main/java/android/basics/SimpleReturnResultActivity.java
+++ app/src/main/java/android/basics/SimpleReturnResultActivity.java
@@ -1,9 +1,12 @@
 package android.basics;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
@@ -20,5 +23,13 @@ public class SimpleReturnResultActivity extends Activity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_simple_return_result,menu);
         return true;
+    }
+
+    public void sendResult(View v){
+        String result = ((EditText)findViewById(R.id.txtRandomResultText)).getText().toString();
+        Intent i = new Intent();
+        i.putExtra("result",result);
+        setResult(RESULT_OK,i);
+        finish();
     }
 }
