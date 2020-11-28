f76a8bfb02aae02f1732a0ba77149648bc272774
Dominik277 <dominikdoric277@gmail.com>
2020-11-28 21:51:13 +0100

    ImplicitIntentsActivity.

--- app/src/main/java/android/basics/ImplicitIntentsActivity.java
+++ app/src/main/java/android/basics/ImplicitIntentsActivity.java
@@ -1,4 +1,50 @@
 package android.basics;
 
-public class ImplicitIntentsActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class ImplicitIntentsActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_implicit_intents);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_implicit_intents,menu);
+        return true;
+    }
+
+    public void visitUrlAddress(View v){
+        Uri url = getUriToVisit();
+        if (url != null){
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(url);
+            startActivity(i);
+        }
+    }
+
+    private Uri getUriToVisit() {
+        String urlAddress = ((TextView)findViewById(R.id.txtUrlAddress)).getText().toString();
+        if (urlAddress != null){
+            if (!urlAddress.startsWith("http://")){
+                urlAddress = "http://" + urlAddress;
+            }
+            return Uri.parse(urlAddress);
+        }else {
+            return null;
+        }
+    }
+
 }
