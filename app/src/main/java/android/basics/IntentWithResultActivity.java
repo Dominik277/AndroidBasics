554be4e8036a6b55a45abc1c6bd5c3b12c7b7679
Dominik277 <dominikdoric277@gmail.com>
2020-11-28 22:18:13 +0100

    IntentWithResultActivity.

--- app/src/main/java/android/basics/IntentWithResultActivity.java
+++ app/src/main/java/android/basics/IntentWithResultActivity.java
@@ -1,4 +1,45 @@
 package android.basics;
 
-public class IntentWithResultActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class IntentWithResultActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_with_result);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_intent_with_result,menu);
+        return true;
+    }
+
+    public void enterText(View v){
+        startActivityForResult(new Intent
+                (IntentWithResultActivity.this,SimpleReturnResultActivity.class)
+                ,GET_RESULT_TEXT);
+    }
+
+    protected void onActivityResult(int requstCode, int resultCode, Intent data){
+        if (requstCode == 0){
+            if (requstCode == RESULT_OK){
+                TextView tvResult = findViewById(R.id.txtDisplayResult);
+                tvResult.setText(data.getStringExtra("result"));
+                Toast.makeText(this,data.getStringExtra("result"),Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
