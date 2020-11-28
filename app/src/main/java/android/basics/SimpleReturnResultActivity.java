ddca20cc5554abbefa7c9ddbb9a46d5f342a3897
Dominik277 <dominikdoric277@gmail.com>
2020-11-28 22:30:30 +0100

    SimpleReturnResultActivity onCreateOptionMenu dodan.

--- app/src/main/java/android/basics/SimpleReturnResultActivity.java
+++ app/src/main/java/android/basics/SimpleReturnResultActivity.java
@@ -1,4 +1,24 @@
 package android.basics;
 
-public class SimpleReturnResultActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import androidx.annotation.Nullable;
+
+public class SimpleReturnResultActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_return_result);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_simple_return_result,menu);
+        return true;
+    }
 }
