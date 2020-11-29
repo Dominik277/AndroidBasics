be355fe60b58bcf670605e43ac360e73038c65e0
Dominik277 <dominikdoric277@gmail.com>
2020-11-29 18:57:39 +0100

    LinearLayoutDemoActivity.

---
+++ app/src/main/java/android/basics/LinearLayoutDemoActivity.java
@@ -0,0 +1,22 @@
+package android.basics;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class LinearLayoutDemoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_linear_layout, menu);
+        return true;
+    }
+
+}
